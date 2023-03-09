fbf92c4010463a4d6ad76a51bbf116735254118c
Ruven <ruven@uni-bremen.de>
2023-03-09 22:27:53 +0100

    Implemented InfestedMudBrick, Fixed LootTable

---
+++ src/main/java/net/Pandarix/betterarcheology/block/custom/InfestedMudBricks.java
@@ -0,0 +1,12 @@
+package net.Pandarix.betterarcheology.block.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.InfestedBlock;
+
+public class InfestedMudBricks extends InfestedBlock {
+
+    public InfestedMudBricks(Block regularBlock, Settings settings) {
+        super(regularBlock, settings);
+    }
+
+}
