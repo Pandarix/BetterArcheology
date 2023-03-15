aae58680fa3c7a20762871d6710e33f30777d85a
Ruven <ruven@uni-bremen.de>
2023-03-15 01:35:13 +0100

    added future loottable injection

--- src/main/java/net/Pandarix/betterarcheology/BetterArcheology.java
+++ src/main/java/net/Pandarix/betterarcheology/BetterArcheology.java
@@ -5,6 +5,7 @@ import net.Pandarix.betterarcheology.block.entity.ModBlockEntities;
 import net.Pandarix.betterarcheology.item.ModItemGroup;
 import net.Pandarix.betterarcheology.item.ModItems;
 import net.Pandarix.betterarcheology.screen.ModScreenHandlers;
+import net.Pandarix.betterarcheology.util.ModLootTableModifiers;
 import net.Pandarix.betterarcheology.villager.ModVillagers;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
@@ -32,5 +33,6 @@ public class BetterArcheology implements ModInitializer {
 		ModScreenHandlers.registerAllScreenHandlers(); //registers all Screen-Handlers
 		ModVillagers.registerVillagers(); //registers all Villagers from BetterArcheology
 		ModVillagers.registerTrades(); //registers all Villager Trades
+		ModLootTableModifiers.modifyLootTables();	//TODO: enable when 1.20 loottables are active
 	}
 }
