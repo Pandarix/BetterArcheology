fde5dc408962e9ff81905384efdaf3f79a2a8259
Ruven <ruven@uni-bremen.de>
2023-03-16 17:28:41 +0100

    fixed Serverside ScreenHandler issue

--- src/main/java/net/Pandarix/betterarcheology/screen/ModScreenHandlers.java
+++ src/main/java/net/Pandarix/betterarcheology/screen/ModScreenHandlers.java
@@ -1,13 +1,16 @@
 package net.Pandarix.betterarcheology.screen;
 
+import net.Pandarix.betterarcheology.BetterArcheology;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.resource.featuretoggle.FeatureFlags;
-import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.util.Identifier;
 
 public class ModScreenHandlers {
-    public static ScreenHandlerType<IdentifyingScreenHandler> IDENTIFYING_SCREEN_HANDLER;
+    public static ScreenHandlerType<IdentifyingScreenHandler> IDENTIFYING_SCREEN_HANDLER = new ScreenHandlerType<>(IdentifyingScreenHandler::new, FeatureFlags.VANILLA_FEATURES);
 
-    public static void registerAllScreenHandlers() {
-        IDENTIFYING_SCREEN_HANDLER = new ScreenHandlerType<>(IdentifyingScreenHandler::new, FeatureFlags.VANILLA_FEATURES);            //Feature Flag declares screen to be accessible by default
+    public static void registerAllScreenHandlers(){
+        Registry.register(Registries.SCREEN_HANDLER, new Identifier(BetterArcheology.MOD_ID, "archeology_table"), IDENTIFYING_SCREEN_HANDLER);
     }
 }
