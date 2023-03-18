67d54b345a270577f998c2989d88d953f56510c8
Ruven <ruven@uni-bremen.de>
2023-03-18 15:57:11 +0100

    added recipes, lang & sounds
    TODO: Fence Gate model missing

--- src/main/java/net/Pandarix/betterarcheology/BetterArcheologyClient.java
+++ src/main/java/net/Pandarix/betterarcheology/BetterArcheologyClient.java
@@ -1,15 +1,22 @@
 package net.Pandarix.betterarcheology;
 
+import net.Pandarix.betterarcheology.block.ModBlocks;
 import net.Pandarix.betterarcheology.screen.IdentifyingScreen;
 import net.Pandarix.betterarcheology.screen.ModScreenHandlers;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.client.render.RenderLayer;
 
 public class BetterArcheologyClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HandledScreens.register(ModScreenHandlers.IDENTIFYING_SCREEN_HANDLER, IdentifyingScreen::new);
+
+        //RENDERING
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ROTTEN_DOOR, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.ROTTEN_TRAPDOOR, RenderLayer.getCutout());
     }
 
 }
--- src/main/java/net/Pandarix/betterarcheology/block/entity/ArcheologyTableBlockEntity.java
+++ src/main/java/net/Pandarix/betterarcheology/block/entity/ArcheologyTableBlockEntity.java
@@ -1,24 +1,37 @@
 package net.Pandarix.betterarcheology.block.entity;
 
 import net.Pandarix.betterarcheology.BetterArcheology;
+import net.Pandarix.betterarcheology.block.ModBlocks;
 import net.Pandarix.betterarcheology.item.ModItems;
 import net.Pandarix.betterarcheology.screen.IdentifyingScreenHandler;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.SuspiciousSandBlockEntity;
+import net.minecraft.command.argument.ParticleEffectArgumentType;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.BrushItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.particle.BlockStateParticleEffect;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.PropertyDelegate;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -110,6 +123,9 @@ public class ArcheologyTableBlockEntity extends BlockEntity implements NamedScre
         }
 
         if (hasRecipe(entity)) {                                      //if the entity has a recipe inside:
+            if (entity.progress % 10 == 0) {
+                world.playSound(null, entity.pos, SoundEvents.ITEM_BRUSH_BRUSHING, SoundCategory.BLOCKS, 0.25f, 1f);
+            }
             entity.progress++;
             markDirty(world, blockPos, blockState);
             if (entity.progress >= entity.maxProgress) {              //if crafting progress is bigger or as big as the maxProgress, then craft the Item, else reset the timer
@@ -130,9 +146,28 @@ public class ArcheologyTableBlockEntity extends BlockEntity implements NamedScre
 
         if (hasRecipe(entity)) {
             entity.removeStack(1, 1);
-            entity.getStack(0).setDamage(entity.getStack(0).getDamage()-1);                                             //TODO: Balance Damage taken
-            entity.setStack(2, new ItemStack(Items.ACACIA_FENCE, entity.getStack(2).getCount() + 1));
-            entity.resetProgress();                                                                                                                    //TODO: Replace Output
+            ItemStack brush = entity.getStack(0);
+
+            int newDamage = brush.getDamage() + 1;//calculate new Damage Value the item would have
+            //if the item is supposed to break or the durability is smaller than zero
+            if (newDamage > brush.getMaxDamage()) {
+                entity.removeStack(0, 1);   //remove the Item
+                assert entity.world != null;
+                if (!entity.world.isClient()) {
+                    //play break sound
+                    entity.world.playSound(null, entity.pos, SoundEvents.ENTITY_ITEM_BREAK, SoundCategory.BLOCKS, 0.25f, 1f);
+                }
+            } else {
+                //if not, set the damage to the calculated damage above
+                brush.setDamage(newDamage);    //TODO: Balance Damage taken
+            }
+
+            if (!entity.world.isClient()) {
+                //play sound after crafting
+                entity.world.playSound(null, entity.pos, SoundEvents.ITEM_BRUSH_BRUSH_SAND_COMPLETED, SoundCategory.BLOCKS, 0.5f, 1f);
+            }
+            entity.setStack(2, new ItemStack(Items.ACACIA_FENCE, entity.getStack(2).getCount() + 1));    //set crafted output in the output slot, TODO: Replace Output
+            entity.resetProgress(); //resets crafting progress
         }
 
     }
@@ -156,5 +191,16 @@ public class ArcheologyTableBlockEntity extends BlockEntity implements NamedScre
 
     private static boolean canInsertAmountIntoOutputSlot(SimpleInventory inventory) {                           //defines canInsertAmountIntoOutputSlot
         return inventory.getStack(2).getMaxCount() > inventory.getStack(2).getCount();
+    }
+
+    //TODO implement correctly
+    private static void addDustParticles(World world, BlockEntity entity) {
+        int i = world.getRandom().nextBetweenExclusive(7, 12);
+        BlockStateParticleEffect blockStateParticleEffect = new BlockStateParticleEffect(ParticleTypes.BLOCK, ModBlocks.ARCHEOLOGY_TABLE.getDefaultState());
+        BlockPos pos = entity.getPos();
+
+        for (int j = 0; j < i; ++j) {
+            world.addParticle(blockStateParticleEffect, pos.getX() + 0.5, pos.getY() + 1, pos.getZ() + 0.5, world.getRandom().nextDouble(), 0.1, world.getRandom().nextDouble());
+        }
     }
 }
