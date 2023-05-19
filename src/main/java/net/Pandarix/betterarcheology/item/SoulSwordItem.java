4fff76ae0b1f82b782ecb01b110bf213be7162a0
Ruven <ruven@uni-bremen.de>
2023-05-19 15:44:18 +0200

    Added SoulSword

---
+++ src/main/java/net/Pandarix/betterarcheology/item/SoulSwordItem.java
@@ -0,0 +1,103 @@
+package net.Pandarix.betterarcheology.item;
+
+import net.Pandarix.betterarcheology.BetterArcheology;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileUtil;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SoulSwordItem extends SwordItem {
+    public SoulSwordItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+        super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public int getMaxUseTime(ItemStack stack) {
+        return 6;
+    }
+
+    @Override
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.BLOCK;
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack itemStack = user.getStackInHand(hand);
+        user.setCurrentHand(hand);
+        return TypedActionResult.consume(itemStack);
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        if (!(user instanceof PlayerEntity player)) {
+            return stack;
+        }
+
+        HitResult hitresult = ProjectileUtil.getCollision(player, Predicate.not(Entity::isSpectator), Math.sqrt(ServerPlayNetworkHandler.MAX_BREAK_SQUARED_DISTANCE) - 1.0);
+
+        if (hitresult instanceof EntityHitResult entityHitResult) {
+            if (entityHitResult.getType() == HitResult.Type.ENTITY) {
+
+                Entity entity = entityHitResult.getEntity();
+                if (entity.isAttackable()) {
+                    if (world.isClient()) {
+                        Vec3d playerPos = player.getPos();
+                        Vec3d targetPos = entity.getPos();
+                        Vec3d toPlayerPos = playerPos.subtract(targetPos);
+                        BetterArcheology.LOGGER.info("Movement: " + toPlayerPos);
+                        for (float f = 0; f <= 1; f += 0.1) {
+                            world.addParticle(ParticleTypes.SCULK_SOUL,
+                                    lerp(playerPos.x, targetPos.x, f),
+                                    lerp(playerPos.y, targetPos.y, f) + 1,
+                                    lerp(playerPos.z, targetPos.z, f),
+                                    toPlayerPos.x * f / 15, toPlayerPos.y * f / 15, toPlayerPos.z * f / 15);
+                        }
+                    } else {
+                        world.playSoundFromEntity(null, player, SoundEvents.ENTITY_MULE_EAT, SoundCategory.PLAYERS, 0.5f, 1f);
+                        world.playSoundFromEntity(null, player, SoundEvents.ENTITY_WITHER_SHOOT, SoundCategory.PLAYERS, 0.1f, 0.25f);
+                        entity.damage(entity.getDamageSources().playerAttack(player), this.getAttackDamage() / 2);
+                        player.heal(this.getAttackDamage() / 2);
+                        player.getItemCooldownManager().set(this, 120);
+                        stack.damage(1, user, (p) -> {
+                            p.sendToolBreakStatus(player.getActiveHand());
+                        });
+                    }
+                }
+
+            }
+        }
+        return super.finishUsing(stack, world, user);
+    }
+
+    private double lerp(double a, double b, float f) {
+        return a + f * (b - a);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        super.appendTooltip(stack, world, tooltip, context);
+        tooltip.add(Text.translatable(this.getTranslationKey()+"_description").formatted(Formatting.DARK_AQUA));
+    }
+}
