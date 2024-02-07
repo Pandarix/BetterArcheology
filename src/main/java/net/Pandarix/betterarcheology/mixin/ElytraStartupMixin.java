2cec818320a1dae45f3684a4a9ffe1bfb1480e48
Ruven <ruven@uni-bremen.de>
2024-02-07 23:45:30 +0100

    Trinkets (ElytraSlot compat)

--- src/main/java/net/Pandarix/betterarcheology/mixin/ElytraStartupMixin.java
+++ src/main/java/net/Pandarix/betterarcheology/mixin/ElytraStartupMixin.java
@@ -1,30 +1,28 @@
 package net.Pandarix.betterarcheology.mixin;
 
-import net.Pandarix.betterarcheology.enchantment.ModEnchantments;
+import net.Pandarix.betterarcheology.util.ArtifactEnchantmentHelper;
 import net.Pandarix.betterarcheology.util.ModConfigs;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
-public abstract class ElytraStartupMixin {
-    @Shadow public abstract ItemStack getEquippedStack(EquipmentSlot slot);
-
+public abstract class ElytraStartupMixin
+{
     @Inject(method = "startFallFlying", at = @At(value = "TAIL"))
-    private void injectMethod(CallbackInfo ci){
+    private void injectMethod(CallbackInfo ci)
+    {
         //if it is enabled in the config and the chestslot is enchanted
-        if(ModConfigs.ARTIFACT_ENCHANTMENTS_ENABLED && this.getEquippedStack(EquipmentSlot.CHEST).hasEnchantments()){
-            //if the enchantment in the chestslot is soaring winds
-            if(EnchantmentHelper.getLevel(ModEnchantments.SOARING_WINDS, this.getEquippedStack(EquipmentSlot.CHEST)) == 1){
+        if (ModConfigs.ARTIFACT_ENCHANTMENTS_ENABLED)
+        {
+            PlayerEntity betterarcheology$player = (PlayerEntity) (Object) this;
 
-                PlayerEntity betterarcheology$player = (PlayerEntity) (Object) this;
+            //if the enchantment in the chestslot is soaring winds
+            if (ArtifactEnchantmentHelper.hasSoaringWinds(betterarcheology$player))
+            {
                 float betterarcheology$boost = ModConfigs.SOARING_WINDS_BOOST * 0.5f;
                 Vec3d betterarcheology$vec3d = betterarcheology$player.getRotationVector();
                 Vec3d betterarcheology$vec3d2 = betterarcheology$player.getVelocity();
@@ -32,7 +30,7 @@ public abstract class ElytraStartupMixin {
                 //add player velocity when starting to fall-fly
                 betterarcheology$player.setVelocity(betterarcheology$vec3d2.add(
                         betterarcheology$vec3d.x * 0.1 + (betterarcheology$vec3d.x * 1.5 - betterarcheology$vec3d2.x) * betterarcheology$boost,
-                        betterarcheology$vec3d.y * 0.1 + (betterarcheology$vec3d.y * 1.5 - betterarcheology$vec3d2.y) * betterarcheology$boost/2,
+                        betterarcheology$vec3d.y * 0.1 + (betterarcheology$vec3d.y * 1.5 - betterarcheology$vec3d2.y) * betterarcheology$boost / 1.8,
                         betterarcheology$vec3d.z * 0.1 + (betterarcheology$vec3d.z * 1.5 - betterarcheology$vec3d2.z) * betterarcheology$boost));
             }
         }
---
+++ src/main/java/net/Pandarix/betterarcheology/util/ArtifactEnchantmentHelper.java
@@ -0,0 +1,54 @@
+package net.Pandarix.betterarcheology.util;
+
+import dev.emi.trinkets.api.TrinketComponent;
+import dev.emi.trinkets.api.TrinketsApi;
+import net.Pandarix.betterarcheology.BetterArcheology;
+import net.Pandarix.betterarcheology.enchantment.ModEnchantments;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ElytraItem;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class ArtifactEnchantmentHelper
+{
+    public static boolean hasSoaringWinds(PlayerEntity player)
+    {
+        if (player == null)
+        {
+            return false;
+        }
+
+        //  if the Item is an Elytra and has Soaring winds, return true
+        if (player.getEquippedStack(EquipmentSlot.CHEST).getItem() instanceof ElytraItem
+                && EnchantmentHelper.getLevel(ModEnchantments.SOARING_WINDS, player.getEquippedStack(EquipmentSlot.CHEST)) >= 1)
+        {
+            return true;
+        }
+
+        // IF TRINKETS is installed, check for back-slot (used by ElytraSlot mod)
+        if (FabricLoader.getInstance().isModLoaded("elytraslot"))
+        {
+            // if there is a cape-slot
+            if (TrinketsApi.getPlayerSlots(player).get("chest").getSlots().containsKey("cape"))
+            {
+                // if the player has trinkets Data
+                Optional<TrinketComponent> trinketData = TrinketsApi.getTrinketComponent(player);
+                if (trinketData.isPresent())
+                {
+                    // check for a trinkets slot named "cape" with an ElytraItem with Soaring winds on it
+                    return trinketData.get().getAllEquipped().stream().anyMatch((pair) ->
+                            Objects.equals(pair.getLeft().inventory().getSlotType().getName(), "cape")
+                                    && pair.getRight().getItem() instanceof ElytraItem
+                                    && EnchantmentHelper.getLevel(ModEnchantments.SOARING_WINDS, pair.getRight()) >= 1
+                    );
+                }
+            }
+        }
+        // if nothing succeeded, false
+        return false;
+    }
+}
