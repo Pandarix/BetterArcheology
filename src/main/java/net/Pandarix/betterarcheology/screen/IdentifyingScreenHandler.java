47772e76d9a0fc97c7af73aff982a9e3b03bf102
Ruven <ruven@uni-bremen.de>
2024-02-21 22:17:27 +0100

    added archeology table duplication bug, resolving #50

--- src/main/java/net/Pandarix/betterarcheology/screen/IdentifyingScreenHandler.java
+++ src/main/java/net/Pandarix/betterarcheology/screen/IdentifyingScreenHandler.java
@@ -13,17 +13,20 @@ import net.minecraft.screen.PropertyDelegate;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 
-public class IdentifyingScreenHandler extends ScreenHandler {
+public class IdentifyingScreenHandler extends ScreenHandler
+{
     private final Inventory inventory;
     private final PropertyDelegate propertyDelegate;
 
-    public IdentifyingScreenHandler(int syncId, PlayerInventory inventory) {
+    public IdentifyingScreenHandler(int syncId, PlayerInventory inventory)
+    {
         //size of SimpleInventory has to be same as in Defaulted List of ArcheologyTableBlockEntity;
         //size of ArrayPropertyDelegate has to be the same as the PropertyDelegate of ArcheologyTableBlockEntity (number of ints being tracked)
         this(syncId, inventory, new SimpleInventory(ArcheologyTableBlockEntity.INV_SIZE), new ArrayPropertyDelegate(ArcheologyTableBlockEntity.PROPERTY_DELEGATES));
     }
 
-    public IdentifyingScreenHandler(int syncId, PlayerInventory playerInventory, Inventory inventory, PropertyDelegate delegate) {
+    public IdentifyingScreenHandler(int syncId, PlayerInventory playerInventory, Inventory inventory, PropertyDelegate delegate)
+    {
         super(ModScreenHandlers.IDENTIFYING_SCREEN_HANDLER, syncId);    //creates a new Instance of Screenhandler
         checkSize(inventory, ArcheologyTableBlockEntity.INV_SIZE);
         this.inventory = inventory; //sets the Screens Inventory to the given Inventory
@@ -46,11 +49,13 @@ public class IdentifyingScreenHandler extends ScreenHandler {
         addProperties(delegate);
     }
 
-    public boolean isCrafting() {
+    public boolean isCrafting()
+    {
         return propertyDelegate.get(0) > 0;
     }
 
-    public int getScaledProgress() {
+    public int getScaledProgress()
+    {
         int progress = this.propertyDelegate.get(0);
         int maxProgress = this.propertyDelegate.get(1);                         // Maximum Progress, after reaching: progress done
         int progressArrowSize = 74;                                             // This is the width in pixels of your arrow //TODO: Edit correct size
@@ -59,71 +64,70 @@ public class IdentifyingScreenHandler extends ScreenHandler {
     }
 
     @Override
-    public ItemStack quickMove(PlayerEntity player, int invSlot) {
-        ItemStack newStack = ItemStack.EMPTY;   //defines an empty ItemStack, will be used to return the changed Item in the Slot
-        Slot slot = this.slots.get(invSlot);    //the given InventorySlot
+    public ItemStack quickMove(PlayerEntity player, int slot)
+    {
+        ItemStack itemStack = ItemStack.EMPTY;
+        Slot slot2 = (Slot) this.slots.get(slot);
+        if (slot2 != null && slot2.hasStack())
+        {
+            ItemStack itemStack2 = slot2.getStack();
+            itemStack = itemStack2.copy();
+            if (slot == 2)
+            {
+                if (!this.insertItem(itemStack2, 3, this.slots.size() - 1, true))
+                {
+                    return ItemStack.EMPTY;
+                }
 
-        //If the to-be-moved-slot has an Item inside
-        if (slot != null && slot.hasStack()) {
-            ItemStack originalStack = slot.getStack();  //stores the Item in the to-be-moved-slot
-            newStack = originalStack.copy();    //sets a new Stack to the given Item
-
-            //BRUSHES
-            if (originalStack.getItem() instanceof BrushItem) {
-                if (isInInv(invSlot)) {
-                    //inventory -> slots
-                    if (!this.insertItem(originalStack, 0, this.slots.size(), true)) {
+                slot2.onQuickTransfer(itemStack2, itemStack);
+            } else if (slot != 1 && slot != 0)
+            {
+                if (itemStack2.getItem() instanceof BrushItem)
+                {
+                    if (!this.insertItem(itemStack2, 0, 1, false))
+                    {
                         return ItemStack.EMPTY;
                     }
-                    //slots -> inventory
-                } else if (!this.insertItem(originalStack, 0, this.inventory.size(), false)) {
-                    return ItemStack.EMPTY;
-                }
-            }
-
-            //ARTIFACTS
-            if (originalStack.isOf(ModItems.UNIDENTIFIED_ARTIFACT)) {
-                if (isInInv(invSlot)) {
-                    //inventory -> slots
-                    if (!this.insertItem(originalStack, 1, this.slots.size(), true)) {
+                } else if (itemStack2.isOf(ModItems.UNIDENTIFIED_ARTIFACT))
+                {
+                    if (!this.insertItem(itemStack2, 1, 2, false))
+                    {
                         return ItemStack.EMPTY;
                     }
-                    //slots -> inventory
-                } else if (!this.insertItem(originalStack, 1, this.inventory.size(), false)) {
-                    return ItemStack.EMPTY;
                 }
-            }
-
-            if (isInInv(invSlot)) {
-                if (!this.insertItem(originalStack, this.inventory.size(), this.slots.size(), true)) {
-                    return ItemStack.EMPTY;
-                }
-            } else {
-                slot.markDirty();
+            } else if (!this.insertItem(itemStack2, 3, this.slots.size() - 1, false))
+            {
                 return ItemStack.EMPTY;
             }
 
-            //If the to-be-moved-Itemstack is empty, replace the ItemStack with "Empty-Item"
-            if (originalStack.isEmpty()) {
-                slot.setStack(ItemStack.EMPTY);
+            if (itemStack2.isEmpty())
+            {
+                slot2.setStack(ItemStack.EMPTY);
+            } else
+            {
+                slot2.markDirty();
             }
-            slot.markDirty();
+
+            if (itemStack2.getCount() == itemStack.getCount())
+            {
+                return ItemStack.EMPTY;
+            }
+
+            slot2.onTakeItem(player, itemStack2);
         }
 
-        return newStack;
-    }
-
-    private boolean isInInv(int invSlot) {
-        return invSlot < this.inventory.size();
+        return itemStack;
     }
 
     @Override
-    public boolean canUse(PlayerEntity player) {
+    public boolean canUse(PlayerEntity player)
+    {
         return this.inventory.canPlayerUse(player);
     }
 
     //Helper Method to add Players Inventoryslots to Screen
-    private void addPlayerInventory(PlayerInventory playerInventory) {
+    private void addPlayerInventory(PlayerInventory playerInventory)
+    {
         /*Adding Slots of Main Inventory
                 COL:    COL:    COL:    ...
         ROW:    slot    slot    slot
@@ -138,9 +142,11 @@ public class IdentifyingScreenHandler extends ScreenHandler {
         int inventoryColumns = PlayerInventory.getHotbarSize();
 
         //For every Row in the Inventory
-        for (int i = 0; i < inventoryRows; ++i) {
+        for (int i = 0; i < inventoryRows; ++i)
+        {
             //Add a slot for every Column in the Row
-            for (int l = 0; l < inventoryColumns; ++l) {
+            for (int l = 0; l < inventoryColumns; ++l)
+            {
                 //Numbers are Minecrafts pre-defined offsets due to the textures
                 this.addSlot(new Slot(playerInventory, l + i * 9 + 9, 8 + l * 18, 86 + i * 18));
             }
@@ -148,9 +154,11 @@ public class IdentifyingScreenHandler extends ScreenHandler {
     }
 
     //Helper Method to add Players HotBarSlots to Screen
-    private void addPlayerHotbar(PlayerInventory playerInventory) {
+    private void addPlayerHotbar(PlayerInventory playerInventory)
+    {
         //Adds a new Slot to the Screen for every Slot in the Players Hotbar
-        for (int i = 0; i < PlayerInventory.getHotbarSize(); ++i) {
+        for (int i = 0; i < PlayerInventory.getHotbarSize(); ++i)
+        {
             //Numbers are Minecrafts pre-defined offsets due to the textures
             this.addSlot(new Slot(playerInventory, i, 8 + i * 18, 144));
         }
